71a37f5aa35fa6d65ad3ac0a8a7523ace4f9fa4e
andreaavalos99 <andreaavalos99@gmail.com>
2026-02-02 15:59:16 -0300

    primer vistazo del proyecto

---
+++ magiched/store/MagicHedStoreApplication.java
@@ -0,0 +1,11 @@
+package com.magiched.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicHedStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MagicHedStoreApplication.class, args);
+    }
+}
